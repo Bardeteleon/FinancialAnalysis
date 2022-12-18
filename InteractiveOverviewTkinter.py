c611a2b747ef92d09a931ae8f7ecf64511f7fe9a
Konstantin Gerks Surface <konstantin.gerks@stud.tu-darmstadt.de>
2022-12-18 23:07:41 +0100

    Remove GO button

--- InteractiveOverviewTkinter.py
+++ InteractiveOverviewTkinter.py
@@ -2,6 +2,7 @@
 import datetime
 import tkinter
 import matplotlib
+from typing import List
 from EntryFilter import EntryFilter
 from TimeInterval import MonthInterval, TimeIntervalVariants
 from VisualizeStatement import VisualizeStatement
@@ -18,12 +19,12 @@ class InteractiveOverviewTkinter():
 
         self.interpreted_entries = interpreted_entries
 
-        self.months = list(EntryFilter.balance_per_interval(interpreted_entries, TimeIntervalVariants.MONTH).keys())
+        self.pie_intervals = self.get_available_pie_intervals(TimeIntervalVariants.MONTH)
 
         self.intervals = [str(interval.name) for interval in TimeIntervalVariants]
 
-        self.month_var = tkinter.StringVar(self.master)
-        self.month_var.set(self.months[-1])
+        self.pie_interval_var = tkinter.StringVar(self.master)
+        self.pie_interval_var.set(self.pie_intervals[-1])
 
         self.interval_var = tkinter.StringVar(self.master)
         self.interval_var.set(self.intervals[0])
@@ -31,29 +32,31 @@ class InteractiveOverviewTkinter():
         self.interaction_frame = tkinter.Frame(self.master)
         self.interaction_frame.pack(side=tkinter.TOP, fill=tkinter.X)
 
-        self.month_menu = tkinter.OptionMenu(self.interaction_frame, self.month_var, *self.months)
-        self.month_menu.pack(side=tkinter.LEFT, fill=tkinter.X, expand=1)
+        self.pie_interval_menu = tkinter.OptionMenu(self.interaction_frame, self.pie_interval_var, *self.pie_intervals, command=self.pie_interval_menu_cmd)
+        self.pie_interval_menu.pack(side=tkinter.LEFT, fill=tkinter.X, expand=1)
 
         self.interval_menu = tkinter.OptionMenu(self.interaction_frame, self.interval_var, *self.intervals, command=self.interval_menu_cmd)
         self.interval_menu.pack(side=tkinter.LEFT, fill=tkinter.X, expand=1)
 
-        self.confirm_button = tkinter.Button(self.interaction_frame, text="GO", command=self.confirm_button_cmd)
-        self.confirm_button.pack(side=tkinter.LEFT, fill=tkinter.X, expand=1)
-
-        self.fig = VisualizeStatement.draw_overview(self.interpreted_entries, MonthInterval.from_string(self.month_var.get()))
+        self.fig = VisualizeStatement.draw_overview(self.interpreted_entries, MonthInterval.from_string(self.pie_interval_var.get()))
         self.fig_canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(self.fig, self.master)
         self.fig_canvas.get_tk_widget().pack(side=tkinter.BOTTOM, fill=tkinter.BOTH, expand=1)
 
         self.master.state("zoomed")
         self.master.mainloop()
 
-    def confirm_button_cmd(self):
+    def pie_interval_menu_cmd(self, choice):
         self.fig.clear()
-        self.fig = VisualizeStatement.draw_overview(self.interpreted_entries, MonthInterval.from_string(self.month_var.get()), fig=self.fig)
+        self.fig = VisualizeStatement.draw_overview(self.interpreted_entries, MonthInterval.from_string(self.pie_interval_var.get()), self.get_base_interval(), fig=self.fig)
         self.fig_canvas.draw_idle()
 
     def interval_menu_cmd(self, choice):
-        selected_interval = TimeIntervalVariants[self.interval_var.get()]
         self.fig.clear()
-        self.fig = VisualizeStatement.draw_overview(self.interpreted_entries, MonthInterval.from_string(self.month_var.get()), selected_interval, fig=self.fig)
+        self.fig = VisualizeStatement.draw_overview(self.interpreted_entries, MonthInterval.from_string(self.pie_interval_var.get()), self.get_base_interval(), fig=self.fig)
         self.fig_canvas.draw_idle()
+
+    def get_available_pie_intervals(self, variant : TimeIntervalVariants) -> List[str]:
+        return list(EntryFilter.balance_per_interval(self.interpreted_entries, variant).keys())
+
+    def get_base_interval(self) -> TimeIntervalVariants:
+        return TimeIntervalVariants[self.interval_var.get()]
