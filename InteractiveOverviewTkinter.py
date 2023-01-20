cf1ec2ce29dd81127ba34bff0853458007922261
Konstantin Gerks Surface <konstantin.gerks@stud.tu-darmstadt.de>
2023-01-20 23:06:46 +0100

    Add keybindings to OptionMenus

--- InteractiveOverviewTkinter.py
+++ InteractiveOverviewTkinter.py
@@ -6,10 +6,14 @@ import tkinter.ttk
 import matplotlib
 from typing import List
 from EntryFilter import EntryFilter
+from enum import Enum, auto
 from TimeInterval import MonthInterval, TimeInterval, TimeIntervalVariants
 from Types import InterpretedEntry, Tag
 from VisualizeStatement import VisualizeStatement
 
+class Direction(Enum):
+    UP = auto()
+    DOWN = auto()
 
 class InteractiveOverviewTkinter():
 
@@ -31,7 +35,7 @@ class InteractiveOverviewTkinter():
         self.add_zero_entry_each_month_with_all_tags()
 
         self.pie_intervals = self.get_available_pie_intervals(self.initial_interval_variant)
-        self.interval_variants = (str(interval.name) for interval in TimeIntervalVariants)
+        self.interval_variants = [str(interval.name) for interval in TimeIntervalVariants]
         self.tags = [str(tag.name) for tag in Tag]
         self.overall_balance_type = "Overall Balance"
         self.balance_types = [self.overall_balance_type] + self.tags
@@ -68,6 +72,13 @@ class InteractiveOverviewTkinter():
         self.fig_pies_canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(self.fig_pies, self.master)
         self.fig_pies_canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=True)
 
+        self.master.bind('+', lambda event: self.interval_variant_menu_shift(Direction.UP))
+        self.master.bind('-', lambda event: self.interval_variant_menu_shift(Direction.DOWN))
+        self.master.bind('s', lambda event: self.balance_type_menu_shift(Direction.UP))
+        self.master.bind('w', lambda event: self.balance_type_menu_shift(Direction.DOWN))
+        self.master.bind('d', lambda event: self.pie_interval_menu_shift(Direction.UP))
+        self.master.bind('a', lambda event: self.pie_interval_menu_shift(Direction.DOWN))
+
         self.master.state("zoomed")
         self.master.mainloop()
 
@@ -76,21 +87,7 @@ class InteractiveOverviewTkinter():
         self.fig_pies = VisualizeStatement.get_figure_positive_negative_tag_pies(self.interpreted_entries, self.get_pie_interval(), fig=self.fig_pies)
         self.fig_pies_canvas.draw_idle()
 
-    def interval_variant_menu_cmd(self, choice):
-        self.pie_interval_var.set('')
-        self.pie_intervals = self.get_available_pie_intervals(self.get_interval_variant())
-        self.pie_interval_menu.set_menu(self.pie_intervals[-1], *self.pie_intervals)
-
-        self.update_fig_balance()
-
-        self.fig_pies.clear()
-        self.fig_pies = VisualizeStatement.get_figure_positive_negative_tag_pies(self.interpreted_entries, self.get_pie_interval(), fig=self.fig_pies)
-        self.fig_pies_canvas.draw_idle()
-
     def balance_type_cmd(self, choice):
-        self.update_fig_balance()
-
-    def update_fig_balance(self):
         filtered_entries = self.interpreted_entries
         if self.balance_type_var.get() != self.overall_balance_type:
             filtered_entries = EntryFilter.tag(self.interpreted_entries, Tag[self.balance_type_var.get()])
@@ -98,6 +95,24 @@ class InteractiveOverviewTkinter():
         self.fig_balance = VisualizeStatement.get_figure_balance_per_interval(filtered_entries, self.get_interval_variant(), fig=self.fig_balance)
         self.fig_balance_canvas.draw_idle()
 
+    def interval_variant_menu_cmd(self, choice):
+        self.pie_interval_var.set('')
+        self.pie_intervals = self.get_available_pie_intervals(self.get_interval_variant())
+        self.pie_interval_menu.set_menu(self.pie_intervals[-1], *self.pie_intervals)
+        self.balance_type_cmd(choice)
+        self.pie_interval_menu_cmd(choice)
+
+    def pie_interval_menu_shift(self, direction : Direction):
+        self.shift_curr_selection_of_menu(direction, self.pie_intervals, self.pie_interval_var)
+        self.pie_interval_menu_cmd(None)
+    
+    def balance_type_menu_shift(self, direction : Direction):
+        self.shift_curr_selection_of_menu(direction, self.balance_types, self.balance_type_var)
+        self.balance_type_cmd(None)
+    
+    def interval_variant_menu_shift(self, direction : Direction):
+        self.shift_curr_selection_of_menu(direction, self.interval_variants, self.interval_variant_var)
+        self.interval_variant_menu_cmd(None)
 
     def get_available_pie_intervals(self, variant : TimeIntervalVariants) -> List[str]:
         return list(EntryFilter.balance_per_interval(self.interpreted_entries, variant).keys())
@@ -120,3 +135,12 @@ class InteractiveOverviewTkinter():
                 extended_entries.append(zero_entry)
             extended_entries.append(entry)
         self.interpreted_entries = extended_entries
+
+    def shift_curr_selection_of_menu(self, direction : Direction, menu_data : List[str], menu_var : tkinter.StringVar):
+        curr_index : int = menu_data.index(menu_var.get())
+        if direction == Direction.UP:
+            curr_index += 1
+        elif direction == Direction.DOWN:
+            curr_index -=1
+        if curr_index in range(len(menu_data)):
+            menu_var.set(menu_data[curr_index])
