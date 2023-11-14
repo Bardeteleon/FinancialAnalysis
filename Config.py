9af6fa84b15216e22dcd7ec95c05ec7a5ce70aba
Konstantin Gerks Surface <konstantin.gerks@stud.tu-darmstadt.de>
2023-11-15 00:02:03 +0100

    Read heading from config and prepare for multiple heading definitions

--- Config.py
+++ Config.py
@@ -3,8 +3,15 @@ from dataclasses import dataclass
 from typing import List
 
 @dataclass
+class HeadingConfig:
+    date : List[str]
+    amount : List[str]
+    comment : List[str]
+
+@dataclass
 class Config:
     identifications : List[str]
+    headings : List[HeadingConfig]
 
 def read_config(file_path : str) -> Config:
     return dataconf.load(file_path, Config)
--- RawEntriesFromCsvExtractor.py
+++ RawEntriesFromCsvExtractor.py
@@ -4,6 +4,11 @@ from Config import read_config, Config
 import logging
 import re
 
+@dataclass
+class HeadingIndex:
+    in_csv : int
+    in_config : int
+
 class RawEntriesFromCsvExtractor:
 
     def __init__(self, csv : List[List[str]], config_json_path : str):
@@ -15,20 +20,34 @@ class RawEntriesFromCsvExtractor:
         if len(self.__csv) < 2:
             return
 
-        index_date : int = self.__get_heading_index("Buchungstag")
-        index_amount : int = self.__get_heading_index("Betrag")
-        index_comment : int = self.__get_heading_index("Verwendungszweck")
-        index_posting_text : int = self.__get_heading_index("Buchungstext")
-        index_name_involved_person : int = self.__get_heading_index("Name Zahlungsbeteiligter")
-
         self.__config : Config = read_config(self.__config_json_path)
+
+        self.__heading_index : Optional[HeadingIndex] = self.__find_heading_index() 
+        if self.__heading_index is None:
+            logging.error("No heading index found. Abort!")
+            return
+
+        date_indices : List[Optional[int]] = [self.__find_column_index(column) for column in self.__config.headings[self.__heading_index.in_config].date]
+        amount_indices : List[Optional[int]] = [self.__find_column_index(column) for column in self.__config.headings[self.__heading_index.in_config].amount]
+        comment_indices : List[Optional[int]] = [self.__find_column_index(column) for column in self.__config.headings[self.__heading_index.in_config].comment]
+
+        if None in date_indices:
+            logging.error("Unable to find all date columns")
+            return
+        if None in amount_indices:
+            logging.error("Unable to find all amount columns")
+            return
+        if None in comment_indices:
+            logging.error("Unable to find all comment columns")
+            return
+
         identification = self.__find_identification_by_name()
 
-        for row in self.__csv[1:]:
+        for row in self.__csv[self.__heading_index.in_csv+1:]:
             raw_entry = RawEntry(
-                date = row[index_date],
-                amount = row[index_amount],
-                comment = re.sub("\s+", " ", row[index_posting_text] + " " + row[index_name_involved_person] + " " + row[index_comment]),
+                date = RawEntriesFromCsvExtractor.__get_concatenated_column_content(row, date_indices),
+                amount = RawEntriesFromCsvExtractor.__get_concatenated_column_content(row, amount_indices),
+                comment = re.sub("\s+", " ", RawEntriesFromCsvExtractor.__get_concatenated_column_content(row, comment_indices)),
                 identification = identification,
                 type = StatementType.TRANSACTION)
             self.__raw_entries.append(raw_entry)
@@ -38,13 +57,34 @@ class RawEntriesFromCsvExtractor:
     def get_raw_entries(self) -> List[RawEntry]:
         return self.__raw_entries
 
-    def __get_heading_index(self, heading : str) -> int:
-        index : int = 0
+    def __find_column_index(self, column_heading : str) -> Optional[int]:
+        index : Optional[int] = None
         try:
-            index = self.__csv[0].index(heading)
+            index = self.__csv[self.__heading_index.in_csv].index(column_heading)
         except ValueError:
-            logging.error(f"No {heading} index found in {self.__csv[0]}")
+            logging.error(f"No {column_heading} index found in {self.__csv[self.__heading_index.in_csv]}")
         return index
+
+    def __find_heading_index(self) -> Optional[HeadingIndex]:
+        for heading_index_in_config, heading_config in enumerate(self.__config.headings):
+            all_column_headings : List[str] = []
+            all_column_headings += heading_config.date
+            all_column_headings += heading_config.amount
+            all_column_headings += heading_config.comment
+            all_column_headings_regex = "(" + "|".join(all_column_headings) + f")"
+            logging.debug(all_column_headings_regex)
+            for heading_index_in_csv, row in enumerate(self.__csv):
+                row_as_string = " ".join(row)
+                match = re.findall(all_column_headings_regex, row_as_string)
+                logging.debug(match)
+                logging.debug(len(match))
+                logging.debug(len(all_column_headings))
+                if len(match) == len(all_column_headings):
+                    return HeadingIndex(heading_index_in_csv,heading_index_in_config)
+                if heading_index_in_csv > 10:
+                    return None
+        return None
+
     
     def __find_identification_by_name(self) -> str:
         for i, row in enumerate(self.__csv):
